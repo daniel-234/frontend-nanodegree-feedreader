3e4798c9421c39d54682639355585a28f708406b
Daniele <daniele.rb6@gmail.com>
2017-03-11 22:59:30 +0100

    docs: Update README and rewrite request urls

--- js/app.js
+++ js/app.js
@@ -10,16 +10,16 @@
 var allFeeds = [
     {
         name: 'Udacity Blog',
-        url: 'http://blog.udacity.com/feed'
+        url: 'www://blog.udacity.com/feed'
     }, {
         name: 'CSS Tricks',
-        url: 'http://feeds.feedburner.com/CssTricks'
+        url: 'www://feeds.feedburner.com/CssTricks'
     }, {
         name: 'HTML5 Rocks',
-        url: 'http://feeds.feedburner.com/html5rocks'
+        url: 'www://feeds.feedburner.com/html5rocks'
     }, {
         name: 'Linear Digressions',
-        url: 'http://feeds.feedburner.com/udacity-linear-digressions'
+        url: 'www://feeds.feedburner.com/udacity-linear-digressions'
     }
 ];
 
